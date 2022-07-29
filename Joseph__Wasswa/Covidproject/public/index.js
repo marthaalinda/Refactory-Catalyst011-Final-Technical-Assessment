9aa2079c825c994ffc2898e2e8334c282003ae6a
joseph <wasswajose9@gmail.com>
2022-07-29 12:12:09 +0000

    css files

--- Joseph__Wasswa/Covidproject/public/index.js
+++ Joseph__Wasswa/Covidproject/public/index.js
@@ -1,266 +1,203 @@
-const Validation=()=>{
-    let Sur=document.registration.surname;
-    let checkerror = document.querySelector('.one');
-let Given=document.registration.given;
-    // let check = document.querySelector('.two');
-    let ddate = document.registration.date;
-// let checke = document.querySelector('.three');
-let Residence=document.registration.placeofresidence;
-// let checker = document.querySelector('.four');
-let Occupation=document.registration.occupation;
-// let checkerr = document.querySelector('.five');
-let Nation=document.registration.nationality;
-// let checkerro = document.querySelector('.six');
-let cat = document.registration.category;
-// let checked = document.querySelector('.seven');
+const Validation = () => {  
 
-    const surname = /^[a-zA-Z0-9]+$/;
-    if(Sur.value.length <3 || !Sur.value.match(surname)){
-        Sur.style.border ='2px solid red';
-        checkerror.textContent = 'This field is required'
-        return false;
-}
-else{
-    Sur.style.border ='2px solid green '
-    // return true
-}
-
-// givename
-
-    const givename = /^[a-zA-Z0-9]+$/;
-    if(Given.value.length <3 || !Given.value.match(givename)){
-        Given.style.border ='2px solid red';
-        checkerror.textContent = 'This field is required'
-        return false;
-}
-else{
-    Given.style.border ='2px solid green '
-    // return true
-}
-
-
-if(
-    ddate.value == ''
-
-){checkerror.textContent = 'This field is required'
-    ddate.style.border ='2px Solid red'
+  let Given = document.registration.givenname;
+  let checkerror = document.querySelector(".two");
+  const givename = /^[a-zA-Z0-9]+$/;
+  if (Given.value.length < 3 || !Given.value.match(givename)) {
+    Given.style.border = "2px solid red";
+    checkerror.textContent = "This field is required";
+    checkerror.style.color = "red";
     return false;
-}
-else {
-    ddate.style.border ='2px solid green'
+  } else {
+    Given.style.border = "2px solid green ";
+  }
+
+  // firstname
+
+  let Surname = document.registration.surname;
+  let checkerror1 = document.querySelector(".one");
+  const surname = /^[a-zA-Z0-9]+$/;
+  if (Surname.value.length < 3 || !Surname.value.match(surname)) {
+    Surname.style.border = "2px solid red";
+    checkerror1.textContent = "This field is required";
+    checkerror1.style.color = "red";
+    return false;
+  } else {
+    Surname.style.border = "2px solid green ";
+  }
+
+  // date
+
+  let date = document.registration.date;
+  let checkerror2 = document.querySelector(".three");
+  if (date.value == "") {
+    checkerror2.textContent = "Select date of Birth";
+    checkerror2.style.color = "red";
+    date.style.border = "2px Solid red";
+    return false;
+  } else {
+    date.style.border = "2px solid green";
+  }
+
+  let Place = document.registration.place;
+  let checkerror3 = document.querySelector(".four");
+  const place = /^[a-zA-Z0-9]+$/;
+  if (Place.value.length < 3 || !Place.value.match(place)) {
+    Place.style.border = "2px solid red";
+    checkerror3.textContent = "This field is required";
+    checkerror3.style.color = "red";
+    return false;
+  } else {
+    Place.style.border = "2px solid green ";
+    
+  }
+
+  let Occupation = document.registration.occupation;
+  let checkerror4 = document.querySelector(".five");
+  const Occupants = /^[a-zA-Z0-9]+$/;
+  if (Occupation.value.length < 3 || !Occupation.value.match(Occupants)) {
+    Occupation.style.border = "2px solid red";
+    checkerror4.textContent = "This field is required";
+    checkerror4.style.color = "red";
+    return false;
+  } else {
+    Occupation.style.border = "2px solid green ";
+    
+  }
+
+  let Nation = document.registration.nationality;
+  let checkerror5 = document.querySelector(".six");
+  const Uganda = /^[a-zA-Z0-9]+$/;
+  if (Nation.value.length < 3 || !Nation.value.match(Uganda)) {
+    Nation.style.border = "2px solid red";
+    checkerror5.textContent = "This field is required";
+    checkerror5.style.color = "red";
+    return false;
+  } else {
+    Nation.style.border = "2px solid green ";
     // return true;
-}
+  }
 
-
-const place = /^[a-zA-Z0-9]+$/;
-if(Residence.value.length <3 || !Residence.value.match(place)){
-    Residence.style.border ='2px solid red';
-    checkerror.textContent = 'This field is required'
+  let cat = document.registration.category;
+  let checkerror6 = document.querySelector(".seven");
+  if (cat.value == "") {
+    checkerror6.textContent = "Select Patient category";
+    checkerror6.style.color = "red";
+    cat.style.border = "2px Solid red";
     return false;
-}
-else{
-Residence.style.border ='2px solid green '
-// return true
-}
+  } else {
+    cat.style.border = "2px solid green";
+   
+  }
 
-const Occupants= /^[a-zA-Z0-9]+$/;
-if(Occupation.value.length <3 || !Occupation.value.match(Occupants)){
-    Occupation.style.border ='2px solid red';
-    checkerror.textContent = 'This field is required'
-    return false;
-}
-else{
-Occupation.style.border ='2px solid green '
-// return true
+  return true;
 };
 
-
-const Uganda = /^[a-zA-Z0-9]+$/;
-if(Nation.value.length <3 || !Nation.value.match(Uganda)){
-    Nation.style.border ='2px solid red';
-    checkerror.textContent = 'This field is required'
+// // onblur
+// const firstFocus = () => {
+//   let surname = document.registration.surname.focus();
+// };
+// secondname
+const val2 = () => {
+  let Given = document.registration.givenname;
+  let checkerror = document.querySelector(".two");
+  const givename = /^[a-zA-Z0-9]+$/;
+  if (Given.value.length < 3 || !Given.value.match(givename)) {
+    Given.style.border = "2px solid red";
+    checkerror.textContent = "This field is required";
+    checkerror.style.color = "red";
     return false;
-}
-else{
-Nation.style.border ='2px solid green '
-// return true
-}
-
-
-if(
-    cat.value == ''
-
-){checkerror.textContent = 'This field is required'
-    cat.style.border ='2px Solid red'
+  } else {
+    Given.style.border = "2px solid green ";
+    return true;
+  }
+};
+// firstname
+const val1 = () => {
+  let Surname = document.registration.surname;
+  let checkerror = document.querySelector(".one");
+  const surname = /^[a-zA-Z0-9]+$/;
+  if (Surname.value.length < 3 || !Surname.value.match(surname)) {
+    Surname.style.border = "2px solid red";
+    checkerror.textContent = "This field is required";
+    checkerror.style.color = "red";
     return false;
-}
-else {
-    cat.style.border ='2px solid green'
-    // return true;
-}
-
-
-
-
-return true;
-}
-
-
-
-// onblur
-const firstFocus =()=>{
-    let sale = document.registration.surname.focus();
+  } else {
+    Surname.style.border = "2px solid green ";
+    return true;
+  }
 };
 
-const valSur =() =>{
-    let Sur=document.registration.surname;
-    let checkerror = document.querySelector('.one');
-    const surname = /^[a-zA-Z0-9]+$/;
-    if(Sur.value.length <3 || !Sur.value.match(surname)){
-        Sur.style.border ='2px solid red';
-        checkerror.textContent = 'This field is required'
-        return false;
-}
-else{
-    Sur.style.border ='2px solid green '
-    return true
-}
-}
-
-// Givenname
-const valGi =() =>{
-    let Given=document.registration.given;
-    let checkerror = document.querySelector('.two');
-    const givename = /^[a-zA-Z0-9]+$/;
-    if(Given.value.length <3 || !Given.value.match(givename)){
-        Given.style.border ='2px solid red';
-        checkerror.textContent = 'This field is required'
-        return false;
-}
-else{
-    Given.style.border ='2px solid green '
-    return true
-}
-};
 // date
-const valDate = () =>{
-    let ddate = document.registration.date;
-    let checkerror = document.querySelector('.three');
-    if(
-        ddate.value == ''
-
-    ){checkerror.textContent = 'This field is required'
-        ddate.style.border ='2px Solid red'
-        return false;
-    }
-    else {
-        ddate.style.border ='2px solid green'
-        return true;
-    }
-    
+const val3 = () => {
+  let date = document.registration.date;
+  let checkerror = document.querySelector(".three");
+  if (date.value == "") {
+    checkerror.textContent = "Select date of Birth";
+    checkerror.style.color = "red";
+    date.style.border = "2px Solid red";
+    return false;
+  } else {
+    date.style.border = "2px solid green";
+    return true;
+  }
 };
 
-const valPlace =() =>{
-    let Residence=document.registration.placeofresidence;
-    let checkerror = document.querySelector('.four');
-    const place = /^[a-zA-Z0-9]+$/;
-    if(Residence.value.length <3 || !Residence.value.match(place)){
-        Residence.style.border ='2px solid red';
-        checkerror.textContent = 'This field is required'
-        return false;
-}
-else{
-    Residence.style.border ='2px solid green '
-    return true
-}
+const val4 = () => {
+  let Place = document.registration.place;
+  let checkerror = document.querySelector(".four");
+  const place = /^[a-zA-Z0-9]+$/;
+  if (Place.value.length < 3 || !Place.value.match(place)) {
+    Place.style.border = "2px solid red";
+    checkerror.textContent = "This field is required";
+    checkerror.style.color = "red";
+    return false;
+  } else {
+    Place.style.border = "2px solid green ";
+    return true;
+  }
 };
 
-
-const valOccupation =() =>{
-    let Occupation=document.registration.occupation;
-    let checkerror = document.querySelector('.five');
-    const Occupants= /^[a-zA-Z0-9]+$/;
-    if(Occupation.value.length <3 || !Occupation.value.match(Occupants)){
-        Occupation.style.border ='2px solid red';
-        checkerror.textContent = 'This field is required'
-        return false;
-}
-else{
-    Occupation.style.border ='2px solid green '
-    return true
-}
+const val5 = () => {
+  let Occupation = document.registration.occupation;
+  let checkerror = document.querySelector(".five");
+  const Occupants = /^[a-zA-Z0-9]+$/;
+  if (Occupation.value.length < 3 || !Occupation.value.match(Occupants)) {
+    Occupation.style.border = "2px solid red";
+    checkerror.textContent = "This field is required";
+    checkerror.style.color = "red";
+    return false;
+  } else {
+    Occupation.style.border = "2px solid green ";
+    return true;
+  }
 };
 
-
-const valNation =() =>{
-    let Nation=document.registration.nationality;
-    let checkerror = document.querySelector('.six');
-    const Uganda = /^[a-zA-Z0-9]+$/;
-    if(Nation.value.length <3 || !Nation.value.match(Uganda)){
-        Nation.style.border ='2px solid red';
-        checkerror.textContent = 'This field is required'
-        return false;
-}
-else{
-    Nation.style.border ='2px solid green '
-    return true
-}
+const val6 = () => {
+  let Nation = document.registration.nationality;
+  let checkerror = document.querySelector(".six");
+  const Uganda = /^[a-zA-Z0-9]+$/;
+  if (Nation.value.length < 3 || !Nation.value.match(Uganda)) {
+    Nation.style.border = "2px solid red";
+    checkerror.textContent = "This field is required";
+    checkerror.style.color = "red";
+    return false;
+  } else {
+    Nation.style.border = "2px solid green ";
+    return true;
+  }
 };
 
-
-const valCat = () =>{
-    let cat = document.registration.category;
-    let checkerror = document.querySelector('.seven');
-    if(
-        cat.value == ''
-
-    ){checkerror.textContent = 'This field is required'
-        cat.style.border ='2px Solid red'
-        return false;
-    }
-    else {
-        cat.style.border ='2px solid green'
-        return true;
-    }
-    
+const val7 = () => {
+  let cat = document.registration.category;
+  let checkerror = document.querySelector(".seven");
+  if (cat.value == "") {
+    checkerror.textContent = "Select Patient category";
+    checkerror.style.color = "red";
+    cat.style.border = "2px Solid red";
+    return false;
+  } else {
+    cat.style.border = "2px solid green";
+    return true;
+  }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // const valNin =() =>{
-    //     const nin = document.registration.nationalid;
-    //     const ninValid = /^[a-zA-Z0-9]+$/;
-    //     if(nin.value.match(ninValid) && nin.value.length == 14){
-    //         nin.style.border ='2px solid white';
-    //         return true; 
-    //     }
-    //     else{
-    //         nin.style.border='2px solid red';
-    //         return false;
-    //     }
-    // };
