2529865952a7b53178e4bb74f1eacdc39b6af0f9
eugenekisaakye <eugene.kisaakye@gmail.com>
2022-07-29 14:08:37 +0300

    phase5

--- Eugene-Kisaakye/project-folder/index.js
+++ Eugene-Kisaakye/project-folder/index.js
@@ -4,14 +4,19 @@ const mongoose = require("mongoose");
 const app = express();
 const router = express.Router();
 
+app.use(express.static('public'))
+
+
 mongoose.connect("mongodb://localhost:27017/work", () => {
   console.log("Connected to database!");
 });
 
-router.get('/',(req,res) =>{
-  res.sendFile()
+app.set
+
+app.get('/',(req,res) =>{
+  res.sendFile('/Eugene-Kisaakye/project-folder/public/index.html')
 })
 
-// app.listen(4000, () => {
-//   console.log("server started on port 4000");
-// });
+app.listen(4000, () => {
+  console.log("server started on port 4000");
+});
