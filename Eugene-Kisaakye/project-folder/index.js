c093afd0eca50416ef5377f3a6ae4df8077c478e
eugenekisaakye <eugene.kisaakye@gmail.com>
2022-07-29 12:17:05 +0300

    phase2

--- Eugene-Kisaakye/project-folder/index.js
+++ Eugene-Kisaakye/project-folder/index.js
@@ -1,13 +1,17 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
 const app = express();
+const router = express.Router();
 
-mongoose.connect("mongodb://localhost:27017/work",() => {
-    console.log("Connected to database!");
-  });
-
-
-app.listen(4000,()=>{
-    console.log('server started on port 4000')
+mongoose.connect("mongodb://localhost:27017/work", () => {
+  console.log("Connected to database!");
 });
+
+router.get('/',(req,res) =>{
+  res.sendFile()
+})
+
+// app.listen(4000, () => {
+//   console.log("server started on port 4000");
+// });
